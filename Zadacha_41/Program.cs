10078741ab0e2b3ba4a9902e743abbb7547e9c33
Yana <wyemelt@gmail.com>
2022-12-28 21:30:58 +0300

    Zadacha_41 done

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int vvod(int number)
+{
+    int count = 0;
+    while (number != 0)
+    {
+        if (number > 0) count++;
+        number = int.Parse(Console.ReadLine());
+    }
+    return count;
+}
+
+Console.WriteLine("Введите числа, для прекращения ввода введите - 0");
+int res = vvod(int.Parse(Console.ReadLine()));
+Console.WriteLine($"Колличество чисел больше 0 равно {res}");
