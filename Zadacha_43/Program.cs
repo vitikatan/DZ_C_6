330ffd72415f357e5e728f285503ffc328dad4cd
Yana <wyemelt@gmail.com>
2022-12-28 22:01:54 +0300

    Zadacha_43 done

---
+++ Zadacha_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// y = k1 * x + b1, y = k2 * x + b2;
+Console.Clear();
+double straight(double i)
+{
+    return i;
+}
+
+Console.WriteLine("Введите k1");
+double k1 = straight(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("Введите k2");
+double k2 = straight(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("Введите b1");
+double b1 = straight(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("Введите b2");
+double b2 = straight(double.Parse(Console.ReadLine()));
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Прямые пересекуться в точке ({x};{y})");
